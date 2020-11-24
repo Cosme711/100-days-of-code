c97fab71487ac9b370c0142ccc7afa5b42c65212
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2020-11-24 19:35:37 +0100

    Update Day 0

---
+++ Day0 - App Windbnb & transition/src/components/Modal.vue
@@ -0,0 +1,37 @@
+<template>
+<transition name="fade">
+    <div v-if="show">
+      <slot></slot>
+    </div>
+</transition>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    },
+    overflow: {
+      type: Boolean,
+      default: false
+    }
+  },
+  watch: {
+    show() {
+      if (this.overflow) document.body.style.overflow = this.show ? 'hidden' : 'visible'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to  {
+  opacity: 0;
+}
+</style>
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+# I've joined the #100DaysOfCode Challenge
 # 100 Days Of Code - Log
 
 
